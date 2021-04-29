15326eb6ed8104058ad68b455834661fd4915999
Tobias Anhalt <tobiasanhalt@Tobiass-MacBook-Pro.local>
2021-04-29 22:33:10 +0300

    added task descriptions to the task

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -11,9 +11,169 @@ use Illuminate\Support\Facades\Date;
 
 class EventsController extends BaseController
 {
+    /*
+     Requirements:
+    - maximum 2 sql queries
+    - verify your solution with `php artisan test`
+    - do a `git commit && git push` after you are done or when the time limit is over
+
+    Hints:
+    - open the `app/Http/Controllers/EventsController` file
+    - partial or not working answers also get graded so make sure you commit what you have
+
+    Sample response on GET /events:
+    ```json
+    [
+        {
+            "id": 1,
+            "name": "Laravel convention 2020",
+            "created_at": "2021-04-25T09:32:27.000000Z",
+            "updated_at": "2021-04-25T09:32:27.000000Z",
+            "workshops": [
+                {
+                    "id": 1,
+                    "start": "2020-02-21 10:00:00",
+                    "end": "2020-02-21 16:00:00",
+                    "event_id": 1,
+                    "name": "Illuminate your knowledge of the laravel code base",
+                    "created_at": "2021-04-25T09:32:27.000000Z",
+                    "updated_at": "2021-04-25T09:32:27.000000Z"
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "name": "Laravel convention 2021",
+            "created_at": "2021-04-25T09:32:27.000000Z",
+            "updated_at": "2021-04-25T09:32:27.000000Z",
+            "workshops": [
+                {
+                    "id": 2,
+                    "start": "2021-10-21 10:00:00",
+                    "end": "2021-10-21 18:00:00",
+                    "event_id": 2,
+                    "name": "The new Eloquent - load more with less",
+                    "created_at": "2021-04-25T09:32:27.000000Z",
+                    "updated_at": "2021-04-25T09:32:27.000000Z"
+                },
+                {
+                    "id": 3,
+                    "start": "2021-11-21 09:00:00",
+                    "end": "2021-11-21 17:00:00",
+                    "event_id": 2,
+                    "name": "AutoEx - handles exceptions 100% automatic",
+                    "created_at": "2021-04-25T09:32:27.000000Z",
+                    "updated_at": "2021-04-25T09:32:27.000000Z"
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "name": "React convention 2021",
+            "created_at": "2021-04-25T09:32:27.000000Z",
+            "updated_at": "2021-04-25T09:32:27.000000Z",
+            "workshops": [
+                {
+                    "id": 4,
+                    "start": "2021-08-21 10:00:00",
+                    "end": "2021-08-21 18:00:00",
+                    "event_id": 3,
+                    "name": "#NoClass pure functional programming",
+                    "created_at": "2021-04-25T09:32:27.000000Z",
+                    "updated_at": "2021-04-25T09:32:27.000000Z"
+                },
+                {
+                    "id": 5,
+                    "start": "2021-08-21 09:00:00",
+                    "end": "2021-08-21 17:00:00",
+                    "event_id": 3,
+                    "name": "Navigating the function jungle",
+                    "created_at": "2021-04-25T09:32:27.000000Z",
+                    "updated_at": "2021-04-25T09:32:27.000000Z"
+                }
+            ]
+        }
+    ]
+     */
+
     public function getEventsWithWorkshops() {
         throw new \Exception('implement in coding task 1');
     }
+
+
+    /*
+    Requirements:
+    - only events that have not yet started should be included
+    - the event starting time is determined by the first workshop of the event
+    - the eloquent expressions should result in maximum 3 SQL queries, no matter the amount of events
+    - all filtering of records should happen in the database
+    - verify your solution with `php artisan test`
+    - do a `git commit && git push` after you are done or when the time limit is over
+
+    Hints:
+    - open the `app/Http/Controllers/EventsController` file
+    - partial or not working answers also get graded so make sure you commit what you have
+    - join, whereIn, min, groupBy, havingRaw might be helpful
+    - in the sample data set  the event with id 1 is already in the past and should therefore be excluded
+
+    Sample response on GET /futureevents:
+    ```json
+    [
+        {
+            "id": 2,
+            "name": "Laravel convention 2021",
+            "created_at": "2021-04-20T07:01:14.000000Z",
+            "updated_at": "2021-04-20T07:01:14.000000Z",
+            "workshops": [
+                {
+                    "id": 2,
+                    "start": "2021-10-21 10:00:00",
+                    "end": "2021-10-21 18:00:00",
+                    "event_id": 2,
+                    "name": "The new Eloquent - load more with less",
+                    "created_at": "2021-04-20T07:01:14.000000Z",
+                    "updated_at": "2021-04-20T07:01:14.000000Z"
+                },
+                {
+                    "id": 3,
+                    "start": "2021-11-21 09:00:00",
+                    "end": "2021-11-21 17:00:00",
+                    "event_id": 2,
+                    "name": "AutoEx - handles exceptions 100% automatic",
+                    "created_at": "2021-04-20T07:01:14.000000Z",
+                    "updated_at": "2021-04-20T07:01:14.000000Z"
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "name": "React convention 2021",
+            "created_at": "2021-04-20T07:01:14.000000Z",
+            "updated_at": "2021-04-20T07:01:14.000000Z",
+            "workshops": [
+                {
+                    "id": 4,
+                    "start": "2021-08-21 10:00:00",
+                    "end": "2021-08-21 18:00:00",
+                    "event_id": 3,
+                    "name": "#NoClass pure functional programming",
+                    "created_at": "2021-04-20T07:01:14.000000Z",
+                    "updated_at": "2021-04-20T07:01:14.000000Z"
+                },
+                {
+                    "id": 5,
+                    "start": "2021-08-21 09:00:00",
+                    "end": "2021-08-21 17:00:00",
+                    "event_id": 3,
+                    "name": "Navigating the function jungle",
+                    "created_at": "2021-04-20T07:01:14.000000Z",
+                    "updated_at": "2021-04-20T07:01:14.000000Z"
+                }
+            ]
+        }
+    ]
+    ```
+     */
 
     public function getFutureEventsWithWorkshops() {
         throw new \Exception('implement in coding task 2');
--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -8,10 +8,90 @@ use Illuminate\Routing\Controller as BaseController;
 
 class MenuController extends BaseController
 {
-    /**
-     * SAMPLE RESPONSE:
-     *
-     *
+    /*
+    Requirements:
+    - the eloquent expressions should result in EXACTLY one SQL query no matter the nesting level or the amount of menu items.
+    - it should work for infinite level of depth (children of childrens children of childrens children, ...)
+    - verify your solution with `php artisan test`
+    - do a `git commit && git push` after you are done or when the time limit is over
+
+    Hints:
+    - open the `app/Http/Controllers/MenuController` file
+    - eager loading cannot load deeply nested relationships
+    - a recursive function in php is needed to structure the query results
+    - partial or not working answers also get graded so make sure you commit what you have
+
+
+    Sample response on GET /menu:
+    ```json
+    [
+        {
+            "id": 1,
+            "name": "All events",
+            "url": "/events",
+            "parent_id": null,
+            "created_at": "2021-04-27T15:35:15.000000Z",
+            "updated_at": "2021-04-27T15:35:15.000000Z",
+            "children": [
+                {
+                    "id": 2,
+                    "name": "Laracon",
+                    "url": "/events/laracon",
+                    "parent_id": 1,
+                    "created_at": "2021-04-27T15:35:15.000000Z",
+                    "updated_at": "2021-04-27T15:35:15.000000Z",
+                    "children": [
+                        {
+                            "id": 3,
+                            "name": "Illuminate your knowledge of the laravel code base",
+                            "url": "/events/laracon/workshops/illuminate",
+                            "parent_id": 2,
+                            "created_at": "2021-04-27T15:35:15.000000Z",
+                            "updated_at": "2021-04-27T15:35:15.000000Z",
+                            "children": []
+                        },
+                        {
+                            "id": 4,
+                            "name": "The new Eloquent - load more with less",
+                            "url": "/events/laracon/workshops/eloquent",
+                            "parent_id": 2,
+                            "created_at": "2021-04-27T15:35:15.000000Z",
+                            "updated_at": "2021-04-27T15:35:15.000000Z",
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "id": 5,
+                    "name": "Reactcon",
+                    "url": "/events/reactcon",
+                    "parent_id": 1,
+                    "created_at": "2021-04-27T15:35:15.000000Z",
+                    "updated_at": "2021-04-27T15:35:15.000000Z",
+                    "children": [
+                        {
+                            "id": 6,
+                            "name": "#NoClass pure functional programming",
+                            "url": "/events/reactcon/workshops/noclass",
+                            "parent_id": 5,
+                            "created_at": "2021-04-27T15:35:15.000000Z",
+                            "updated_at": "2021-04-27T15:35:15.000000Z",
+                            "children": []
+                        },
+                        {
+                            "id": 7,
+                            "name": "Navigating the function jungle",
+                            "url": "/events/reactcon/workshops/jungle",
+                            "parent_id": 5,
+                            "created_at": "2021-04-27T15:35:15.000000Z",
+                            "updated_at": "2021-04-27T15:35:15.000000Z",
+                            "children": []
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
      */
 
     public function getMenuItems() {
