0b90635dccaa0cbfe7153b111e7517dbde5f28dc
Tobias Anhalt <tobiasanhalt@Tobiass-MacBook-Pro.local>
2021-04-29 22:23:11 +0300

    fixxed seed data for the menu question

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -66,21 +66,47 @@ class DatabaseSeeder extends Seeder
             ]);
 
             $rootItem = MenuItem::create([
-               'name' => 'All events',
-               'url' => '/events',
+                'name' => 'All events',
+                'url' => '/events',
             ]);
 
-            MenuItem::create([
+            $laraconItem = MenuItem::create([
                 'name' => 'Laracon',
                 'url' => '/events/laracon',
                 'parent_id' => $rootItem->id
             ]);
 
             MenuItem::create([
+                'name' => 'Illuminate your knowledge of the laravel code base',
+                'url' => '/events/laracon/workshops/illuminate',
+                'parent_id' => $laraconItem->id
+            ]);
+
+            MenuItem::create([
+                'name' => 'The new Eloquent - load more with less',
+                'url' => '/events/laracon/workshops/eloquent',
+                'parent_id' => $laraconItem->id
+            ]);
+
+            $reactconItem = MenuItem::create([
                 'name' => 'Reactcon',
                 'url' => '/events/reactcon',
                 'parent_id' => $rootItem->id
             ]);
+
+            MenuItem::create([
+                'name' => '#NoClass pure functional programming',
+                'url' => '/events/reactcon/workshops/noclass',
+                'parent_id' => $reactconItem->id
+            ]);
+
+            MenuItem::create([
+                'name' => 'Navigating the function jungle',
+                'url' => '/events/reactcon/workshops/jungle',
+                'parent_id' => $reactconItem->id
+            ]);
+
+
         });
     }
 }
