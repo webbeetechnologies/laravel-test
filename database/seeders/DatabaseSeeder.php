3556b5e43d7102fadd86169c524e4434277f0e93
Tobias <github@mythli.net>
2022-05-26 20:12:11 +0300

    make test timeless

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,9 @@ namespace Database\Seeders;
 use App\Models\MenuItem;
 use App\Models\Workshop;
 use App\Models\Event;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
@@ -15,98 +17,108 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+
+    protected function seedMenu() {
+        $rootItem = MenuItem::create([
+            'name' => 'All events',
+            'url' => '/events',
+        ]);
+
+        $laraconItem = MenuItem::create([
+            'name' => 'Laracon',
+            'url' => '/events/laracon',
+            'parent_id' => $rootItem->id
+        ]);
+
+        MenuItem::create([
+            'name' => 'Illuminate your knowledge of the laravel code base',
+            'url' => '/events/laracon/workshops/illuminate',
+            'parent_id' => $laraconItem->id
+        ]);
+
+        MenuItem::create([
+            'name' => 'The new Eloquent - load more with less',
+            'url' => '/events/laracon/workshops/eloquent',
+            'parent_id' => $laraconItem->id
+        ]);
+
+        $reactconItem = MenuItem::create([
+            'name' => 'Reactcon',
+            'url' => '/events/reactcon',
+            'parent_id' => $rootItem->id
+        ]);
+
+        MenuItem::create([
+            'name' => '#NoClass pure functional programming',
+            'url' => '/events/reactcon/workshops/noclass',
+            'parent_id' => $reactconItem->id
+        ]);
+
+        MenuItem::create([
+            'name' => 'Navigating the function jungle',
+            'url' => '/events/reactcon/workshops/jungle',
+            'parent_id' => $reactconItem->id
+        ]);
+    }
+
+    protected function seedEvents() {
+        $date = (new Carbon())->subYear()->setDay(21);
+
+        $lcon1 = Event::create([
+            'name' => 'Laravel convention '.$date->year
+        ]);
+
+        Workshop::create([
+            'start' => $date->clone()->setMonth(2)->setHour(10),
+            'end' => $date->clone()->setMonth(2)->setHour(16),
+            'name' => 'Illuminate your knowledge of the laravel code base',
+            'event_id' => $lcon1->id
+        ]);
+
+        $date = (new Carbon())->addYears(1);
+
+        $lcon2 = Event::create([
+            'name' => 'Laravel convention '.$date->year
+        ]);
+
+        Workshop::create([
+            'start' => $date->clone()->setMonth(10)->setHour(10),
+            'end' => $date->clone()->setMonth(10)->setHour(16),
+            'name' => 'The new Eloquent - load more with less',
+            'event_id' => $lcon2->id
+        ]);
+
+        Workshop::create([
+            'start' => $date->clone()->setMonth(11)->setHour(10),
+            'end' => $date->clone()->setMonth(11)->setHour(17),
+            'name' => 'AutoEx - handles exceptions 100% automatic',
+            'event_id' => $lcon2->id
+        ]);
+
+        $rcon = Event::create([
+            'name' => 'React convention '.$date->year
+        ]);
+
+        Workshop::create([
+            'start' => $date->clone()->setMonth(8)->setHour(10),
+            'end' => $date->clone()->setMonth(8)->setHour(18),
+            'name' => '#NoClass pure functional programming',
+            'event_id' => $rcon->id
+        ]);
+
+        Workshop::create([
+            'start' => $date->clone()->setMonth(11)->setHour(9),
+            'end' => $date->clone()->setMonth(11)->setHour(17),
+            'name' => 'Navigating the function jungle',
+            'event_id' => $rcon->id
+        ]);
+    }
+
     public function run()
     {
         DB::transaction(function($table) {
-            $lcon1 = Event::create([
-                'name' => 'Laravel convention 2020'
-            ]);
-
-            Workshop::create([
-                'start' => '2020/02/21 10:00',
-                'end' => '2020/02/21 16:00',
-                'name' => 'Illuminate your knowledge of the laravel code base',
-                'event_id' => $lcon1->id
-            ]);
-
-            $lcon2 = Event::create([
-                'name' => 'Laravel convention 2021'
-            ]);
-
-            Workshop::create([
-                'start' => '2021/10/21 10:00',
-                'end' => '2021/10/21 18:00',
-                'name' => 'The new Eloquent - load more with less',
-                'event_id' => $lcon2->id
-            ]);
-
-            Workshop::create([
-                'start' => '2021/11/21 09:00',
-                'end' => '2021/11/21 17:00',
-                'name' => 'AutoEx - handles exceptions 100% automatic',
-                'event_id' => $lcon2->id
-            ]);
-
-            $rcon = Event::create([
-                'name' => 'React convention 2021'
-            ]);
-
-            Workshop::create([
-                'start' => '2021/08/21 10:00',
-                'end' => '2021/08/21 18:00',
-                'name' => '#NoClass pure functional programming',
-                'event_id' => $rcon->id
-            ]);
-
-            Workshop::create([
-                'start' => '2021/08/21 09:00',
-                'end' => '2021/08/21 17:00',
-                'name' => 'Navigating the function jungle',
-                'event_id' => $rcon->id
-            ]);
-
-            $rootItem = MenuItem::create([
-                'name' => 'All events',
-                'url' => '/events',
-            ]);
-
-            $laraconItem = MenuItem::create([
-                'name' => 'Laracon',
-                'url' => '/events/laracon',
-                'parent_id' => $rootItem->id
-            ]);
-
-            MenuItem::create([
-                'name' => 'Illuminate your knowledge of the laravel code base',
-                'url' => '/events/laracon/workshops/illuminate',
-                'parent_id' => $laraconItem->id
-            ]);
-
-            MenuItem::create([
-                'name' => 'The new Eloquent - load more with less',
-                'url' => '/events/laracon/workshops/eloquent',
-                'parent_id' => $laraconItem->id
-            ]);
-
-            $reactconItem = MenuItem::create([
-                'name' => 'Reactcon',
-                'url' => '/events/reactcon',
-                'parent_id' => $rootItem->id
-            ]);
-
-            MenuItem::create([
-                'name' => '#NoClass pure functional programming',
-                'url' => '/events/reactcon/workshops/noclass',
-                'parent_id' => $reactconItem->id
-            ]);
-
-            MenuItem::create([
-                'name' => 'Navigating the function jungle',
-                'url' => '/events/reactcon/workshops/jungle',
-                'parent_id' => $reactconItem->id
-            ]);
-
-
+            $this->seedEvents();
+            $this->seedMenu();
         });
     }
 }
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,41 +3,50 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Carbon;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
     public function testWarmupEvents() {
+        $datePast = (new Carbon())->subYear()->setDay(21);
+        $dateFuture = (new Carbon())->addYears(1);
+
         $response = $this->get('/warmupevents');
         $response->assertStatus(200)
             ->assertJsonCount(3)
-            ->assertJsonPath('0.name', 'Laravel convention 2020')
-            ->assertJsonPath('1.name', 'Laravel convention 2021')
-            ->assertJsonPath('2.name', 'React convention 2021');
+            ->assertJsonPath('0.name', 'Laravel convention '.$datePast->year)
+            ->assertJsonPath('1.name', 'Laravel convention '.$dateFuture->year)
+            ->assertJsonPath('2.name', 'React convention '.$dateFuture->year);
     }
 
     public function testEvents() {
+        $datePast = (new Carbon())->subYear()->setDay(21);
+        $dateFuture = (new Carbon())->addYears(1);
+
         $response = $this->get('/events');
         $response->assertStatus(200)
             ->assertJsonCount(3)
-            ->assertJsonPath('0.name', 'Laravel convention 2020')
+            ->assertJsonPath('0.name', 'Laravel convention '.$datePast->year)
             ->assertJsonPath('0.workshops.0.name', 'Illuminate your knowledge of the laravel code base')
-            ->assertJsonPath('1.name', 'Laravel convention 2021')
+            ->assertJsonPath('1.name', 'Laravel convention '.$dateFuture->year)
             ->assertJsonPath('1.workshops.0.name', 'The new Eloquent - load more with less')
             ->assertJsonPath('1.workshops.1.name', 'AutoEx - handles exceptions 100% automatic')
-            ->assertJsonPath('2.name', 'React convention 2021')
+            ->assertJsonPath('2.name', 'React convention '.$dateFuture->year)
             ->assertJsonPath('2.workshops.0.name', '#NoClass pure functional programming')
             ->assertJsonPath('2.workshops.1.name', 'Navigating the function jungle');
     }
 
     public function testFutureEvents() {
+        $dateFuture = (new Carbon())->addYears(1);
+
         $response = $this->get('/futureevents');
         $response->assertStatus(200)
             ->assertJsonCount(2)
-            ->assertJsonPath('0.name', 'Laravel convention 2021')
+            ->assertJsonPath('0.name', 'Laravel convention '.$dateFuture->year)
             ->assertJsonPath('0.workshops.0.name', 'The new Eloquent - load more with less')
             ->assertJsonPath('0.workshops.1.name', 'AutoEx - handles exceptions 100% automatic')
-            ->assertJsonPath('1.name', 'React convention 2021')
+            ->assertJsonPath('1.name', 'React convention '.$dateFuture->year)
             ->assertJsonPath('1.workshops.0.name', '#NoClass pure functional programming')
             ->assertJsonPath('1.workshops.1.name', 'Navigating the function jungle');
     }
